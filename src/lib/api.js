a85ec5df834e8de723ef21cb550101474a3d69fc
MCris29 <crismax0629@gmail.com>
2022-07-06 09:04:37 -0500

    Lista de contÃactos

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,5 +4,5 @@ axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 export default axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
+  // withCredentials: true,
 });
