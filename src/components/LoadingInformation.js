4a63b5fd168f5f5c5ffaebc7bcbe2b2f7028c3e3
MCris29 <crismax0629@gmail.com>
2022-11-08 17:50:32 -0500

    Fix style Loading component

--- src/components/LoadingInformation.js
+++ src/components/LoadingInformation.js
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import {Grid, Typography} from "@mui/material";
+import * as React from "react";
+import styles from "@/styles/LoadingInformation.module.css";
+import Box from "@mui/material/Box";
+import { Grid, Typography } from "@mui/material";
 import LinearProgress from "@mui/material/LinearProgress";
 
 export default function LoadingInformation() {
-    return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ height: '90vh', textAlign: "center", padding:{mobile:"10%"} }}
-        >
-            <Grid item xs={3}>
-
-                <Typography variant="h4" color="primary">
-                    <span>Cargando información</span>
-                </Typography>
-                <Box sx={{ width: '100%', paddingTop: "20px"}}>
-                    <LinearProgress />
-                </Box>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      className={styles.container}
+    >
+      <Grid item xs={3}>
+        <Typography variant="h4" color="primary">
+          <span>Cargando información</span>
+        </Typography>
+        <Box sx={{ width: "100%", paddingTop: "20px" }}>
+          <LinearProgress />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
